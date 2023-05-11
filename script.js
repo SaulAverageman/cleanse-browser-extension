1b96c4fa1463de43af85e9118da263d1b173a0d5
sitting duck <bubai.apps@outlook.com>
2023-05-12 03:22:01 +0530

    null void

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 blocked_sites=new Set();
-blocked_sites.add('instagram.com')
-blocked_sites.add('facebook.com')
+blocked_sites.add('www.instagram.com')
+blocked_sites.add('www.facebook.com')
 
 const currentUrl = window.location.href;
 const urlObject = new URL(currentUrl);
 const domainName = urlObject.hostname;
-
+console.log(domainName)
 if (blocked_sites.has(domainName)) {
     window.location.href = "https://m.media-amazon.com/images/I/51o+xu99W9L._AC_SL1000_.jpg";
 }
