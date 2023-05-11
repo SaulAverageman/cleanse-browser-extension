9f24d628937b8669feebab432edb8a4c693f144d
sitting duck <bubai.apps@outlook.com>
2023-05-12 03:33:15 +0530

    null

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ blocked_sites=new Set();
 blocked_sites.add('www.instagram.com')
 blocked_sites.add('www.facebook.com')
 
-const currentUrl = window.location.href;
-const urlObject = new URL(currentUrl);
-const domainName = urlObject.hostname;
-console.log(domainName)
-if (blocked_sites.has(domainName)) {
+const mycurrentUrl = window.location.href;
+const myurlObject = new URL(mycurrentUrl);
+const mydomainName = myurlObject.hostname;
+
+if (blocked_sites.has(mydomainName)) {
     window.location.href = "https://m.media-amazon.com/images/I/51o+xu99W9L._AC_SL1000_.jpg";
 }
   
